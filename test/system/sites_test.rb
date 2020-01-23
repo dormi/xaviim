95e72d4b6dbb25ea594d361d8073aabd4e574028
Xavi Vila <xavivm@gmail.com>
2020-01-23 13:31:13 +0000

    some system tests

---
+++ test/system/sites_test.rb
@@ -0,0 +1,49 @@
+require "application_system_test_case"
+
+class SitesTest < ApplicationSystemTestCase
+  OK_TEXT = "This page can be read though /example?key=secret"
+  KO_TEXT = "You may need to authenticate to see this page, or the page doesn't exist at all."
+
+  test "visiting a site with a valid key works" do
+    visit "/example?key=secret"
+  
+    assert_text OK_TEXT
+  end
+
+  test "visiting a site without key is forbidden" do
+    visit "/example"
+  
+    assert_text KO_TEXT
+  end
+  
+  test "visiting a site with invalid key is forbidden" do
+    visit '/example?key=wrong'
+  
+    assert_text KO_TEXT
+  end
+
+  test "visiting a site wo/ key works after a first visit with key" do
+    visit '/example?key=secret'  
+    visit '/example'
+
+    assert_text OK_TEXT
+  end
+
+  test "visiting an unknown site without key is forbidden" do
+    visit '/unknown'
+  
+    assert_text KO_TEXT
+  end
+
+  test "visiting an unknown site with unexisting key is forbidden" do
+    visit '/unknown?key=wrong'
+  
+    assert_text KO_TEXT
+  end
+
+  test "visiting an unknown site with existing is forbidden" do
+    visit '/unknown?key=secret'
+  
+    assert_text KO_TEXT
+  end
+end
