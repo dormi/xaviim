576d02eb08af7ca844f6979f41ebb3fad7dcf946
Xavi Vila <xavivm@gmail.com>
2020-01-30 13:07:57 +0000

    first syntax highlight test using rouge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def rouge(text, language)
+    # formatter = Rouge::Formatters::HTML.new(css_class: 'highlight')
+    # lexer = Rouge::Lexer.find(language)
+    # formatter.format(lexer.lex(text))
+
+    formatter = Rouge::Formatters::HTML.new
+    lexer = Rouge::Lexer.find(language)
+    formatter.format(lexer.lex(text))
+    # lexer = Rouge::Lexers::Shell.new
+    # formatter.format(lexer.lex(source))
+  end
 end
